feddf85d0cf567a47c6d996af99ee1dfd2dea6c5
Lyon <lyon@Lyons-MacBook-Pro.local>
2015-05-17 17:00:56 +0800

    add test project

---
+++ Clsmap.Wuliao.Mvc.Tests/Controllers/PostControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+using Clsmap.Wuliao.Mvc.Controllers;
+using Clsmap.Wuliao.Mvc.Models;
+
+namespace Clsmap.Wuliao.Mvc.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Detail()
+        {
+            // Arrange
+            var controller = new PostController();
+
+            // Act
+            var result = (ViewResult)controller.Detail();
+            var model = (PostViewModel)result.Model;
+
+            // Assert
+            Assert.AreEqual("Lyon Chen", model.Writer.Name);
+            Assert.AreEqual(1, model.Comments.Count);
+        }
+    }
+}
